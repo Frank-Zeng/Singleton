8ca3a5a6564fe36a493d388040c3377c8214df47
Frank Zeng <zengfgouyt@outlook.com>
2018-10-14 23:03:42 +0800

    add generic cache.

---
+++ SingletonDemo/GenericCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    /// <summary>
+    /// 每个不同的T,都会生产一份不同的副本,适用不同的类型
+    /// 适合不同类型，需要缓存一份数据的场景，效率高
+    /// （效率高于静态字典的缓存）
+    /// 不能清除缓存
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericCache<T>
+    {
+        private static string TypeTime = "";
+        static GenericCache()
+        {
+            Console.WriteLine("This is generic cache construct.");
+            TypeTime = string.Format("{0}_{1}",typeof(T).FullName, DateTime.Now.ToString("yyyyMMddHHmmssss"));
+        }
+
+        public static string GetCache()
+        {
+            return TypeTime;
+        }
+    }
+}
--- SingletonDemo/Program.cs
+++ SingletonDemo/Program.cs
@@ -7,6 +7,14 @@ namespace SingletonDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(GenericCache<int>.GetCache());
+                    Console.WriteLine(GenericCache<long>.GetCache());
+                    Console.WriteLine(GenericCache<string>.GetCache());
+                }
+            }
         }
     }
 }
